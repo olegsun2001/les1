1468e00566b071aad5cc762862a0ef2398c143b6
Oleg Ponikarenko <olegsun2001@gmail.com>
2024-02-14 13:52:46 +0300

    add lesson1:task3

---
+++ c_sharp/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 5;
+int b = 7;
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
