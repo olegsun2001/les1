c02c2c65bac48e59b240e335f5c0bfd27b28caab
Oleg Ponikarenko <olegsun2001@gmail.com>
2024-02-14 13:58:41 +0300

    lesson1:task4

---
+++ c_sharp/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 4;
+int b = 6;
+int c = 12;
+int d = 2;
+int e = 7;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
